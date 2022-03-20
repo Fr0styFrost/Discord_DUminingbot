d67dba5c697d0f054e1143f291463643d9253b48
Heinrich Zinserling <32123959+Fr0styFrost@users.noreply.github.com>
2022-03-20 21:40:26 +0000

    Update miningbot.py

--- miningbot.py
+++ miningbot.py
@@ -228,7 +228,7 @@ async def calib(ctx, muname, calibration):
 #get specific payment info of player
 @bot.command()
 async def getAllPayment(ctx):
-    if "Admin" in [i.name.lower() for i in ctx.author.roles.name]:
+    if "Admin" in [i.name.lower() for i in ctx.author.roles]:
         result = DB.getAllPayment()
         for entry in result:
             await ctx.send('Name: {} , Calibrations done: {}, Payment: {}'.format(entry[0], entry[1], entry[2]))
